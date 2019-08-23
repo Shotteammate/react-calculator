54d17542be15002dd7ff01201d464c2803f86a46
Tik San Pun <shotteammate@gmail.com>
2019-08-23 22:41:21 +0800

    Updates

---
+++ precache-manifest.f9299252e81228c53499b01983939444.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91c1215cb74e3712c1523d4115973c3b",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "4be3324587ba08163ce8",
+    "url": "/react-calculator/static/css/main.38ec8cb1.chunk.css"
+  },
+  {
+    "revision": "a38e9ca1bc71df0d582d",
+    "url": "/react-calculator/static/js/2.5b3c70da.chunk.js"
+  },
+  {
+    "revision": "4be3324587ba08163ce8",
+    "url": "/react-calculator/static/js/main.4b7ddf74.chunk.js"
+  },
+  {
+    "revision": "f098332b43cb68ab616e",
+    "url": "/react-calculator/static/js/runtime~main.1849104f.js"
+  }
+]);
